e087dbfb7d6437328b5e54086f51cc1316591751
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2017-06-21 13:42:04 +0400

    scroll is in-block

--- src/app/shared/loader.component.ts
+++ src/app/shared/loader.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'custom-loader',
-	template: `<div class="loader-wrapper"><div class="loader"></div></div>`
+	template: `
+	<div class="loader-wrapper">
+		<div class="loader">
+		</div>
+	</div>`
 })
 export class LoaderComponent { 
 
